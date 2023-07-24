10078c0996bb9f7949e530b9c074c34014867685
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2023-07-24 13:08:46 -0300

    query_to_json: pre-allocate enough elements

--- src/api.rs
+++ src/api.rs
@@ -111,6 +111,7 @@ pub fn query_to_json(query: &str) -> Result<Vec<pgrx::JsonB>, spi::Error> {
         let queried: Result<(), spi::Error> = Spi::connect(|mut client| {
             let q = format!("select to_jsonb(t) as results from ({query}) t");
             let tup_table = client.update(&q, None, None)?;
+            results.reserve_exact(tup_table.len());
             for row in tup_table {
                 let r = row["results"]
                     .value::<pgrx::JsonB>()
@@ -120,8 +121,10 @@ pub fn query_to_json(query: &str) -> Result<Vec<pgrx::JsonB>, spi::Error> {
             }
             Ok(())
         });
+
         queried
     }));
+
     match queried {
         Ok(_) => Ok(results),
         Err(e) => {
